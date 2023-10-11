3ac548e128be4abe2cefe648c37324536c3bede6
Ramdhannp <ramdhannp6@gmail.com>
2023-10-12 05:31:35 +0700

    final version

--- src/components/CardTables.js
+++ src/components/CardTables.js
@@ -1,70 +1,68 @@
 import React, { useState } from "react";
 
 import {
-  PagingState,
-  IntegratedPaging,
-  SearchState,
-  IntegratedFiltering,
+	PagingState,
+	IntegratedPaging,
+	SearchState,
+	IntegratedFiltering,
 } from "@devexpress/dx-react-grid";
 import {
-  Grid,
-  Table,
-  TableHeaderRow,
-  Toolbar,
-  TableFixedColumns,
-  SearchPanel,
-  PagingPanel,
-  ColumnChooser,
-  TableColumnVisibility,
+	Grid,
+	Table,
+	TableHeaderRow,
+	Toolbar,
+	TableFixedColumns,
+	SearchPanel,
+	PagingPanel,
+	ColumnChooser,
+	TableColumnVisibility,
 } from "@devexpress/dx-react-grid-bootstrap4";
 import "@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css";
 import "../css/tables.css";
 
 const CardTables = (props) => {
-  const [columns] = useState([
-    { name: "id", title: "No" },
-    { name: "full_name", title: "Nama" },
-    { name: "nim", title: "Nim" },
-    { name: "angkatan", title: "Angkatan" },
-    { name: "lokasi_kp", title: "Lokasi" },
-    { name: "pekerjaan", title: "Job Title" },
-    { name: "status", title: "Status" },
-    { name: "hide", title: "hide" },
-  ]);
+	const [columns] = useState([
+		{ name: "full_name", title: "Nama" },
+		{ name: "nim", title: "Nim" },
+		{ name: "angkatan", title: "Angkatan" },
+		{ name: "lokasi_kp", title: "Lokasi" },
+		{ name: "pekerjaan", title: "Job Title" },
+		{ name: "toolTip", title: "Status" },
+		{ name: "status", title: "hide" },
+	]);
 
-  const rows = props.dataTable;
+	const rows = props.dataTable;
 
-  const [tableColumnExtensions] = useState([{ columnName: "id", width: 60 }]);
+	const [tableColumnExtensions] = useState([{ columnName: "id", width: 60 }]);
 
-  const [leftColumns] = useState(["id", "full_name", "hide"]);
-  const [defaultHiddenColumnNames] = useState([
-    "id",
-    "pekerjaan",
-    "lokasi_kp",
-    "hide",
-  ]);
+	const [leftColumns] = useState(["id", "full_name", "hide"]);
+	const [defaultHiddenColumnNames] = useState([
+		"pekerjaan",
+		"lokasi_kp",
+		"status",
+	]);
 
-  return (
-    <div className="card d-flex justify-content-end">
-      <p className="d-none">tabel</p>
-      <Grid rows={rows} columns={columns} columnMinWidth={10}>
-        <SearchState />
-        <IntegratedFiltering />
-        <PagingState pageSize={10} />
-        <IntegratedPaging />
-        <Table columnExtensions={tableColumnExtensions} />
-        <TableHeaderRow />
-        <TableColumnVisibility
-          defaultHiddenColumnNames={defaultHiddenColumnNames}
-        />
-        <TableFixedColumns leftColumns={leftColumns} />
-        <PagingPanel />
-        <Toolbar />
-        <SearchPanel className="justify-content-end" />
-        <ColumnChooser />
-      </Grid>
-    </div>
-  );
+	return (
+		<div className="card d-flex justify-content-end">
+			<p className="d-none">tabel</p>
+			<Grid rows={rows} columns={columns} columnMinWidth={10}>
+				<SearchState />
+				<IntegratedFiltering />
+				<PagingState pageSize={10} />
+				<IntegratedPaging />
+				<Table columnExtensions={tableColumnExtensions} />
+				<TableHeaderRow />
+				<TableColumnVisibility
+					defaultHiddenColumnNames={defaultHiddenColumnNames}
+				/>
+				<TableFixedColumns leftColumns={leftColumns} />
+				<PagingPanel />
+				<Toolbar />
+				<SearchPanel className="justify-content-end" />
+				<ColumnChooser />
+			</Grid>
+		</div>
+	);
 };
 
 export default CardTables;
--- src/components/LineCharts.js
+++ src/components/LineCharts.js
@@ -2,63 +2,63 @@ import ReactApexChart from "react-apexcharts";
 import React from "react";
 
 function LineChart(props) {
-  let kategori = Object.keys(props.selesaiKP || {});
-
-  const series = [
-    {
-      name: "Mahasiswa",
-      data: Object.values(props.selesaiKP || {}),
-    },
-  ];
-  const options = {
-    chart: {
-      id: "chart2",
-      toolbar: {
-        autoSelected: "pan",
-        show: false,
-      },
-      zoom: {
-        enabled: false,
-      },
-    },
-    colors: ["#A8C5DA"],
-    // fill: {
-    //   type: "gradient",
-    //   gradient: {
-    //     opacityFrom: 0.4,
-    //     opacityTo: 0.1,
-    //   },
-    // },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    // title: {
-    //   text: "Mahasiswa yang Sudah KP",
-    //   style: {
-    //     fontSize: 16,
-    //     fontFamily: "Poppins",
-    //     color: "var(--dark-color)",
-    //     fontWeight: 500,
-    //   },
-    // },
-    markers: {
-      size: 0,
-    },
-    xaxis: {
-      categories: kategori,
-    },
-  };
-  return (
-    <ReactApexChart
-      options={options}
-      type="line"
-      series={series}
-      height={350}
-    />
-  );
+	let kategori = Object.keys(props.selesaiKP || {});
+	let jumlah = Object.values(props.selesaiKP || {});
+	const series = [
+		{
+			name: "Mahasiswa",
+			data: jumlah,
+		},
+	];
+	const options = {
+		chart: {
+			id: "chart2",
+			toolbar: {
+				autoSelected: "pan",
+				show: false,
+			},
+			zoom: {
+				enabled: false,
+			},
+		},
+		colors: ["#A8C5DA"],
+		// fill: {
+		//   type: "gradient",
+		//   gradient: {
+		//     opacityFrom: 0.4,
+		//     opacityTo: 0.1,
+		//   },
+		// },
+		dataLabels: {
+			enabled: false,
+		},
+		stroke: {
+			curve: "smooth",
+		},
+		// title: {
+		//   text: "Mahasiswa yang Sudah KP",
+		//   style: {
+		//     fontSize: 16,
+		//     fontFamily: "Poppins",
+		//     color: "var(--dark-color)",
+		//     fontWeight: 500,
+		//   },
+		// },
+		markers: {
+			size: 0,
+		},
+		xaxis: {
+			categories: kategori,
+		},
+	};
+	return (
+		<ReactApexChart
+			options={options}
+			type="line"
+			series={series}
+			height={350}
+		/>
+	);
 }
 
 export default LineChart;
